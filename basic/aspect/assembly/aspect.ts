8f20c6ae850f003b1666a70261374a7f57c481b4
Gene <zhanjun@artela.network>
2023-04-28 11:52:17 +0800

    feat: add new contract for store.js test, please run: node store.js

--- basic/aspect/assembly/aspect.ts
+++ basic/aspect/assembly/aspect.ts
@@ -3,47 +3,49 @@ import {Aspect, Context} from "./lib";
 
 class MyFirstAspect implements Aspect {
     onTxReceive(s: string): string {
-        return Context.lastBlock();
+        let lastBlock = Context.lastBlock().replaceAll("\"", "\\\"");
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"blockLen\":\""+lastBlock+"\"}}";
     }
 
     onBlockInitialize(arg: string): string {
-        return "";
+
+    return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     onTxVerify(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     onAccountVerify(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     onGasPayment(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     preTxExecute(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     preContractCall(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     postContractCall(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     postTxExecute(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     onTxCommit(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 
     onBlockFinalize(arg: string): string {
-        return "";
+        return "{\"success\":true,\"context\":{\"testKey\":\"testValue\",\"testKey2\":\"testValue2\"}}";
     }
 }
 
