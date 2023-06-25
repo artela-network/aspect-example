023072b43f828804b0e6fc7f959b2de35f1bdd79
zhanjun <zhanjun@artela.network>
2023-06-25 17:28:06 +0800

    feat: add reentrance demo

---
+++ reentrance/assembly/index.ts
@@ -0,0 +1,26 @@
+// The entry file of your WebAssembly module.
+
+import { Entry } from "./lib/entry";
+
+import aspect from "./aspect/my_first_aspect"
+import { utils } from "./lib/utils";
+
+let firstAspect = new aspect();
+var entry = new Entry(firstAspect, firstAspect);
+
+
+export function execute(methodPtr: i32, argPtr: i32): i32 {
+  return entry.execute(methodPtr, argPtr);
+}
+
+export function isBlockLevel(): i32 {
+  return entry.isBlockLevel();
+}
+
+export function isTransactionLevel(): i32 {
+  return entry.isTransactionLevel();
+}
+
+export function allocate(size: i32): i32 {
+  return utils.alloc(size);
+}
