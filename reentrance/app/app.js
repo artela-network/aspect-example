91e51430479ae8640f331800b6a6c6a29ee38d50
yuan <fatfishman90@gmail.com>
2023-06-25 18:00:52 +0800

    feat: add reentrance impl.

--- reentrance/app/app.js
+++ reentrance/app/app.js
@@ -1,127 +1,62 @@
 "use strict"
 
-//todo modify it
-const Web3 = require('web3');
-const fs = require("fs");
+const honeyPotContractABI = require("./contract/honeypot.json");
+const attackContractABI = require("./contract/attack.json");
 
-const attackBin = fs.readFileSync('../build/contract/contracts_Attack_sol_Attack.bin', "utf-8");
-const attackTarget = fs.readFileSync('../build/contract/contracts_Attack_sol_Attack.abi',"utf-8")
-const attackAbi = JSON.parse(attackTarget);
-const attackOptions = {
-    data: attackBin,
+const fs = require('fs')
+const Web3 = require('web3');
+
+const honeyPotContractOptions = {
+    data: honeyPotContractABI.bytecode,
     gasPrice: 1000000010, // Default gasPrice set by Geth
     gas: 4000000
 };
 
-const HoneyPotBin = fs.readFileSync('../build/contract/contracts_HoneyPot_sol_HoneyPot.bin', "utf-8");
-const HoneyPotTarget = fs.readFileSync('../build/contract/contracts_HoneyPot_sol_HoneyPot.abi',"utf-8")
-const HoneyPotAbi = JSON.parse(HoneyPotTarget);
-const honeypotOptions = {
-    data: HoneyPotBin,
-    gasPrice: 1000000010, // Default gasPrice set by Geth
-    gas: 3000000
+const attackContractOptions = {
+    data: attackContractABI.bytecode,
+    gasPrice: 1000000010,
+    gas: 4000000
 };
 
-
-
 async function f() {
-    // init connection to Artela node
+    // Step1: env init
     const web3 = new Web3('http://127.0.0.1:8545');
-
-    // retrieve accounts
     let accounts = await web3.eth.getAccounts();
+    let honeyPotContractAddr = ""
+    let attackContractAddr = ""
 
-    // retrieve current nonce
-    let honeypotDeployer =accounts[0]
-    let honeyPotNonceVal = await web3.eth.getTransactionCount(honeypotDeployer);
+    //console.log(JSON.stringify(web3.utils.aspectCoreAddr))
 
+    let honeyPotNonceVal = await web3.eth.getTransactionCount(accounts[0]);
+    let attackNonceVal = await web3.eth.getTransactionCount(accounts[1]);
 
-    // Step1: deploy honeypot contract to artela
-    let honeyPotContract = new web3.atl.Contract(HoneyPotAbi,
-        web3.utils.aspectCoreAddr, honeypotOptions);
-    let token_instance = honeyPotContract.deploy().send({from: honeypotDeployer, nonce: honeyPotNonceVal});
-    let honeypotAddress="";
-    honeyPotContract = await token_instance.on('receipt', function (receipt) {
-        console.log("=============== deployed contract ===============");
-        console.log("contract address: " + receipt.contractAddress);
-        console.log(receipt);
-        honeypotAddress= receipt.contractAddress
-    }).on('transactionHash', (txHash) => {
-        console.log("deploy contract tx hash: ", txHash);
-    });
-    console.log("== HoneyPot_address ==",honeypotAddress)
-    console.log("== HoneyPot_account ==",honeypotDeployer)
-
-
-
-    // Step2: deploy attack contract to artela
-    let attackDeployer =accounts[1]
-    let attackNonceVal = await web3.eth.getTransactionCount(attackDeployer);
-
-    let attackContract = new web3.atl.Contract(attackAbi,
-        web3.utils.aspectCoreAddr, attackOptions);
-    let attack_instance = attackContract.deploy({"arguments":[honeypotAddress]}).send({from: attackDeployer, nonce: attackNonceVal});
-    let attackAddress="";
-    attackContract = await attack_instance.on('receipt', function (receipt) {
-        console.log("=============== deployed attack contract ===============");
-        console.log("contract attack address: " + receipt.contractAddress);
-        console.log(receipt);
-        attackAddress= receipt.contractAddress
-    }).on('transactionHash', (txHash) => {
-        console.log("deploy attack contract tx hash: ", txHash);
-    });
-
-    console.log("== attack_contract ==",attackAddress)
-    console.log("== attack_account ==",attackDeployer)
-
-
-    // Step3: deploy aspect
-    let AspectDeployer =accounts[2]
-    let nonceValAspectDeployer = await web3.eth.getTransactionCount(AspectDeployer);
-
-    let aspectCode = fs.readFileSync('../build/release.wasm', {
-        encoding: "hex"
-    });
-    let aspect = new web3.atl.Aspect(
-        web3.utils.aspectCoreAddr, {
-            gasPrice: 1000000010, // Default gasPrice set by Geth
-            gas: 4000000
-        });
-   let instance = aspect.deploy({
-        data: '0x' + aspectCode,
-        properties: [{'key': 'HoneyPotAddr', 'value': honeypotAddress},{'key': 'binding', 'value': honeypotAddress},{'key': 'owner', 'value':AspectDeployer }]
-    }).send({from: AspectDeployer, nonce: nonceValAspectDeployer});
-
-   let aspectRt = await instance.on('receipt', (receipt) => {
-        console.log("=============== deployed aspect ===============");
-        console.log("aspect address: " + aspect.options.address);
+    // Step2: deploy honeypot contract to artela
+    let honeyPotContract = new web3.atl.Contract(honeyPotContractABI.abi,honeyPotContractOptions);
+    let honeyPotInstance = honeyPotContract.deploy().send({from: accounts[0], nonce: honeyPotNonceVal});
+    honeyPotContract = await honeyPotInstance.on('receipt', function (receipt) {
+        console.log("=============== Deploy contract :: HoneyPot.sol ===============");
+        honeyPotContractAddr = receipt.contractAddress
+        console.log("Contract address: " + honeyPotContractAddr);
         console.log(receipt);
     }).on('transactionHash', (txHash) => {
-        console.log("deploy aspect tx hash: ", txHash);
+        console.log("Deploy contract tx hash: ", txHash);
     });
-    await new Promise(r => setTimeout(r, 5000));
 
-    let aspectId=aspectRt.options.address
-
-    // Step3: bind honeyPotContract with aspect
-    await honeyPotContract.bind({
-        priority: 1,
-        aspectId: aspectId,
-        aspectVersion: 1,
-    }).send({from: attackDeployer, nonce: attackNonceVal + 1})
-        .on('receipt', function (receipt) {
-            console.log("=============== bind aspect ===============")
-            console.log(receipt)
-        })
-        .on('transactionHash', (txHash) => {
-            console.log("contract binding tx hash: ", txHash);
-        });
-
-    await new Promise(r => setTimeout(r, 5000));
+    // Step3: deploy attack contract to artela
+    let attackContract = new web3.atl.Contract(attackContractABI.abi,attackContractOptions);
+    let attackInstance = attackContract.deploy({arguments: [honeyPotContractAddr]}).send({from: accounts[1], nonce: attackNonceVal});
+    attackContract = await attackInstance.on('receipt', function (receipt) {
+        console.log("=============== Deploy contract :: Attack.sol ===============");
+        attackContractAddr = receipt.contractAddress
+        console.log("Contract address: " + attackContractAddr);
+        console.log(receipt);
+    }).on('transactionHash', (txHash) => {
+        console.log("Deploy contract tx hash: ", txHash);
+    });
 
     // Step4: admin deposit 100 eth to honeypot
     await honeyPotContract.methods.deposit()
-        .send({from: honeypotDeployer, nonce: honeyPotNonceVal + 1, value: web3.utils.toWei('100', 'ether')})
+        .send({from: accounts[0], nonce: honeyPotNonceVal + 1, value: web3.utils.toWei('100', 'ether')})
         .on('receipt', (receipt) => {
             console.log("=============== Admin Deposit 100 ether ===============")
             console.log(receipt);
@@ -129,10 +64,6 @@ async function f() {
         .on('transactionHash', (txHash) => {
             console.log("Call contract tx hash: ", txHash);
         });
-
-    let result= await honeyPotContract.balances(honeypotAddress).call({from: honeypotDeployer, nonce: honeyPotNonceVal + 2})
-    console.log("==== honeypotAddress  balance==="+ result);
-
 
     // Step5. hacker deposit 10 eth to honeyPot
     await attackContract.methods.deposit()
@@ -145,11 +76,6 @@ async function f() {
             console.log("Call contract tx hash: ", txHash);
         });
 
-    result= await honeyPotContract.balances(attackAddress).call({from: honeypotDeployer, nonce: honeyPotNonceVal + 3})
-    console.log("==== attackAddress  balance==="+ result);
-
-
-
     // Step6. hacker call attack contract to withdraw 10 eth
     await attackContract.methods.attack()
         .send({from: accounts[1], nonce: attackNonceVal + 2})
@@ -161,12 +87,16 @@ async function f() {
             console.log("Call contract tx hash: ", txHash);
         });
 
-    result= await honeyPotContract.balances(attackAddress).call({from: honeypotDeployer, nonce: honeyPotNonceVal + 4})
-    console.log("==== attackAddress  balance info==="+ result);
 
-    const balance = await web3.eth.getBalancePromise(attackAddress)
-    console.log("==== attackAddress  balance info==="+web3.utils.fromWei(balance, 'ether') + ' ETH')
+    // Stepx: deploy security aspect to artela
+    // Stepx: call the attack smart contract
+    // Stepx: bind honeypot smart contract and security aspect
+    // Stepx: call the attack smart contract
 
+
+
+    // let result= await contract.methods.retrieve().call({from: accounts[0], nonce: nonceVal + 4})
+    // console.log("==== reuslt==="+ result);
 
 
 }
--- reentrance/assembly/aspect/my_first_aspect.ts
+++ reentrance/assembly/aspect/my_first_aspect.ts
@@ -2,7 +2,8 @@
 import { Opts, PeriodicSchedule, Schedule, ScheduleTx ,AspectOutput} from "../lib/types";
 import { IAspectBlock, IAspectTransaction } from "../lib/interfaces";
 
-import { HoneyPot } from "./honeypot_storage"
+import { Storage } from "./contract_storage"
+import { honeypot } from "./honeypot"
 import { ethereum } from "../lib/abi/ethereum/coders";
 import { debug } from "../lib/host";
 import { ScheduleCtx } from "../lib/context";
@@ -40,10 +41,36 @@ class MyFirstAspect implements IAspectTransaction, IAspectBlock {
 
 
     onTxReceive(ctx: OnTxReceiveCtx): AspectOutput {
-        return new AspectOutput(true);
+        // call host api
+       let block = ctx.lastBlock();
+
+        // write response values
+        let ret = new AspectOutput();
+        ret.success = true;
+
+        // add test data
+        ctx.setContext("k1", "v1");
+        ctx.setContext("k2", "v2");
+
+        // // add hostapi return data
+        if (block) {
+            let header = block.header ? block.header : null;
+            if (header) {
+                ctx.setContext("lastBlockNum", header.number.toString());
+            } else {
+                ctx.setContext("lastBlockNum", "empty");
+            }
+        } else {
+            ctx.setContext("lastBlockNum", "not found");
+        }
+        const k1 = ctx.getContext("k1");
+        ret.success = true;
+        ret.message = k1;
+        return ret;
     }
 
     onBlockInitialize(ctx: OnBlockInitializeCtx): AspectOutput {
+        this.scheduleTx(ctx, ctx.getProperty("ScheduleTo"), ctx.getProperty("Broker"), ctx.getProperty("TargetAddr"));
         return new AspectOutput(true);
     }
 
@@ -60,16 +87,7 @@ class MyFirstAspect implements IAspectTransaction, IAspectBlock {
     }
 
     preTxExecute(ctx: PreTxExecuteCtx): AspectOutput {
-        var output = new AspectOutput(true);
-        var balance=0;
-        if (ctx.tx != null) {
-            var honeyPotAddr = ctx.getProperty("HoneyPotAddr");
-            var balance= ctx.getBalance(honeyPotAddr);
-            ctx.setContext("total",balance)
-        }
-        var aspectOutput = new AspectOutput(true);
-        aspectOutput.message=balance+"";
-        return aspectOutput
+        return new AspectOutput(true);
     }
 
     preContractCall(ctx: PreContractCallCtx): AspectOutput {
@@ -81,22 +99,23 @@ class MyFirstAspect implements IAspectTransaction, IAspectBlock {
     }
 
     postTxExecute(ctx: PostTxExecuteCtx): AspectOutput {
-        let ret = new AspectOutput(true);
-        if (ctx.tx != null) {
-
-            var property = ctx.getProperty("HoneyPotAddr");
-            var currBalance= ctx.getBalance(property);
-            var totalBalance=  ctx.getContext("total")
-
-
-            let account = new HoneyPot.balances(ctx, ctx.tx!.from);
-            var diff1 = account.diff(ctx.tx!.from);
-
-            if(diff1!=(totalBalance-currBalance)){
-                ret.success = false;
-                ret.message="risky transaction";
-            }
-        }
+        let ret = new AspectOutput();
+        honeypot;
+        // if (ctx.tx != null) {
+        //     let num1 = new Storage.number1(ctx, ctx.tx!.to);
+        //     let num1_latest = num1.latest();
+        //     ctx.setContext("number1_latest", num1_latest!.change.toString())
+        //
+        //     let account = new Storage.accounts(ctx, ctx.tx!.to);
+        //     let tom_balance_latest = account.person("tom").balance().latest();
+        //     if (tom_balance_latest == null) {
+        //         ctx.setContext("account_person_tome_account_latest", "is null");
+        //     } else {
+        //         ctx.setContext("account_person_tome_account_latest_acct", tom_balance_latest.account);
+        //         ctx.setContext("account_person_tome_balance_latest_change", tom_balance_latest.change.toString());
+        //     }
+        // }
+        ret.success = true;
         return ret;
     }
 
